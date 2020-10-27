5f4d659e46bb898b3177dcaf1c7357f5f8f99c6c
MonikaPatidar <monikapatidar12345@gmail.com>
2020-10-27 14:37:56 +0530

    Changed

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ const Hedr=()=>{
     <div className="header_div">
         <div className="margn_div">
         <ImportContactsIcon style={{fontSize:"80", margin:"0px 0px 0px 30px"}} />
-        <h1 className="heading">KeepNote</h1>
+        <h1 className="heading">KeepNotes</h1>
         </div>
     </div>
     </>
